cd3d3b29c357f59caf26930abf84f280f4836ddc
ionite34 <dev@ionite.io>
2022-05-29 19:23:40 -0400

    Added testing for uread

---
+++ tests/test_uread.py
@@ -0,0 +1,113 @@
+import io
+import csv
+from dataclasses import dataclass
+
+import pytest
+from pytest_lazyfixture import lazy_fixture as fx
+
+from unisafe.uread import uread
+
+expected_raw = """1,"Oh, what is this. This is a system” now, such there.",test
+2,"In these kind of “Cases”, we will do some “tests” like such.",test
+3,"This is a normal sentence, but with ellipsis…",test
+"""
+
+expected_converted = """1,"Oh, what is this. This is a system" now, such there.",test
+2,"In these kind of "Cases", we will do some "tests" like such.",test
+3,"This is a normal sentence, but with ellipsis...",test
+"""
+
+
+@dataclass
+class TestData:
+    raw: str
+
+    def as_io(self) -> io.StringIO:
+        return io.StringIO(self.raw)
+
+    def lines(self) -> list[str]:
+        # Returns a list of lines without newline chars
+        return self.raw.splitlines()
+
+
+@pytest.fixture(scope='module')
+def data_raw():
+    return TestData(expected_raw)
+
+
+@pytest.fixture(scope='module')
+def data_conv():
+    return TestData(expected_converted)
+
+
+@pytest.fixture(scope='function')
+def get_open():
+    return lambda: open('test.csv', 'r', encoding='utf-8')
+
+
+@pytest.fixture(scope='function')
+def get_uread():
+    return lambda: uread('test.csv', escape_files=None)
+
+
+@pytest.fixture(scope='function')
+def get_uread_csv():
+    return lambda: uread('test.csv')
+
+
+# Test Read Usage with Context Manager
+@pytest.mark.parametrize('func, data', [
+    (fx('get_open'), fx('data_raw')),
+    (fx('get_uread'), fx('data_conv'))
+])
+def test_uread_cx_read(func, data):
+    with func() as f:
+        assert f.readable()
+        assert not f.writable()
+        assert not f.closed
+        assert f.read() == data.raw  # Read to joined str
+    with func() as f:
+        assert f.readlines() == data.as_io().readlines()  # Read to list
+    with func() as f:
+        assert f.readline(1) == data.as_io().readline(1)  # Read specific line
+    with func() as f:
+        with pytest.raises(io.UnsupportedOperation):  # Write should not be allowed
+            f.write('test')
+
+
+# Test Read Usage without Context Manager
+@pytest.mark.parametrize('func, data', [
+    (fx('get_open'), fx('data_raw')),
+    (fx('get_uread'), fx('data_conv'))
+])
+def test_uread_read(func, data):
+    f = func()
+    assert isinstance(f, io.TextIOWrapper)
+    assert f.readable()
+    assert f.read() == data.raw
+    assert not f.closed
+    f.close()
+    assert f.closed
+
+
+# Test Read as Iterable
+@pytest.mark.parametrize('func, data', [
+    (fx('get_open'), fx('data_raw')),
+    (fx('get_uread'), fx('data_conv'))
+])
+def test_uread_cx_iter(func, data):
+    with func() as f, data.as_io() as d:
+        for line, expected in zip(f, d):
+            assert line == expected
+
+
+# Test Exceptions
+@pytest.mark.parametrize('func, data', [
+    (lambda x: open(x), fx('data_raw')),
+    (lambda x: uread(x), fx('data_conv'))
+])
+def test_uread_exceptions(func, data):
+    # Invalid file
+    with pytest.raises(FileNotFoundError):
+        with func('not_exist.txt') as f:
+            f.read()
